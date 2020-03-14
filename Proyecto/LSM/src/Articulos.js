207ce94b7748430f9c20247ef0cf8ba066366588
Ariel May <aemd199711@gmail.com>
2020-03-14 12:43:10 -0600

    lista de articulos

---
+++ Proyecto/LSM/src/Articulos.js
@@ -0,0 +1,107 @@
+import { NativeModules } from "react-native"
+
+const articulos = [
+    {
+        nombre: 'Bebidas',
+        articulos:[
+            {
+                id: '1',
+                nombre: 'Coca-Cola',
+                descripcion:'',
+                precio:'10',
+                imagen:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.myamericanmarket.com%2Fes%2Fcoca-cola-classic&psig=AOvVaw3QA4Q7DPWoCL2lcihhby-u&ust=1584212587918000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCdqImSmOgCFQAAAAAdAAAAABAJ'
+            },
+            {
+                id: '2',
+                nombre: 'Pepsi',
+                descripcion:'',
+                precio:'11',
+                imagen:''
+            },
+            {
+                id: '3',
+                nombre: 'Agua',
+                descripcion:'',
+                precio:'12',
+                imagen:''
+            },
+            {
+                id: '4',
+                nombre: 'Jugo Natural',
+                descripcion:'',
+                precio:'13',
+                imagen:''
+            },
+        ]
+
+    },
+    {
+        nombre: 'Comida',
+        articulos:[
+            {
+                id: '5',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '6',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '7',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '8',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+        ]
+
+    },
+    {
+        nombre: 'Botanas',
+        articulos:[
+            {
+                id: '9',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '10',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '11',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+            {
+                id: '12',
+                nombre: '',
+                descripcion:'',
+                precio:'',
+                imagen:''
+            },
+        ]
+
+    },
+]
+export default articulos;
